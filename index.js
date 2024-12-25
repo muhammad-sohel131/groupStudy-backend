8b431a286982907d2c05a267debd8f7496871452
sohel <sohelf131@gmail.com>
2024-12-25 18:47:26 +0600

    adde post and get assignment api

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: [
-      'http://localhost:5173',
-      'https://job-portal-504b7.web.app',
-      'https://job-portal-504b7.firebaseapp.com'
+    'http://localhost:5173',
+    'https://job-portal-504b7.web.app',
+    'https://job-portal-504b7.firebaseapp.com'
   ],
   credentials: true
 }));
@@ -46,8 +46,30 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Express.js server!');
 });
 
+app.get('/assignments', async (req, res) => {
+  try {
+    const cursor = assignmentsCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+  } catch (error) {
+    console.error('Error fetching assignments:', error);
+    res.status(500).send('Error fetching assignments');
+  }
+});
 
+app.post('/assignments', async (req, res) => {
+  try {
+    const data = req.body;
+    const result = await assignmentsCollection.insertOne(data);
+    res.send(result)
+  } catch (e) {
+    console.log(e)
+  }
+})
 
+app.delete('/assignments/:id', async (req, res) => {
+  
+})
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
